832ef27b67309d5e46807f6ce83b96ee5be6ff40
Andrey B <a.borzenets@gmail.com>
2022-12-04 01:09:07 +0300

    The structure has been changed

---
+++ dz_workshop_1/dz_task_2/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.WriteLine("Введите первое число: ");
+string numberOneStr = Console.ReadLine() ?? "";
+Console.WriteLine("Введите второе число: ");
+string numberTwoStr = Console.ReadLine() ?? "";
+int numberOne = Convert.ToInt32(numberOneStr);
+int numberTwo = Convert.ToInt32(numberTwoStr);
+
+if (numberOne > numberTwo)
+{
+    Console.WriteLine($"Число {numberOne} больше числа {numberTwo}");
+}
+else if (numberTwo > numberOne)
+{
+    Console.WriteLine($"Число {numberTwo} больше числа {numberOne}");
+}
+else
+{
+    Console.WriteLine($"Числа {numberOne} и {numberTwo} равны");
+}
