f97e6747178b98b688bc1504ecc2462be8b2141f
Andrey B <a.borzenets@gmail.com>
2022-12-04 01:33:04 +0300

    add dz_task_10

---
+++ dz_workshop_2/dz_task_10/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int GetRandomNumber(int a, int b)
+{
+    Random rnd = new Random();
+    return rnd.Next(a, b);
+}
+
+int GetAverageDigit(int threeDigitNumber)
+{
+    return threeDigitNumber % 100 / 10;
+}
+
+int number = GetRandomNumber(100, 1000);
+Console.WriteLine($"Трехзначное число: {number}");
+Console.WriteLine($"Средняя цифра: {GetAverageDigit(number)}");
