dc5703b8cb962fb819cbb133beae4cb2c14b0915
Андрей Б <a.borzenets@gmail.com>
2022-12-09 14:26:51 +0300

    add dz_task_25

---
+++ dz_workshop_4/dz_task_25/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Задача 25:
+Напишите цикл, который принимает на вход два числа (A и B)
+и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Введите корректное число");
+        }
+    }
+    return result;
+}
+
+int[] numbers = new int[2];
+
+for (int i = 0; i < 2; i++)
+{
+    numbers[i] = GetNumber($"Введите {i+1}-е число");
+}
+
+Console.WriteLine(Math.Pow(numbers[0], numbers[1]));
