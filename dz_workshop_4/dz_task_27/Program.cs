98ffc303c69c6f4ac5d279e8a5dbe92d3f33bef6
Андрей Б <a.borzenets@gmail.com>
2022-12-09 15:24:23 +0300

    dz_task_27 in porigress

--- dz_workshop_4/dz_task_27/Program.cs
+++ dz_workshop_4/dz_task_27/Program.cs
@@ -6,3 +6,46 @@
 82 -> 10
 9012 -> 12
 */
+
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Введите корректное число");
+        }
+    }
+    return result;
+}
+
+int SumDigitsOfNumber(int number)
+{
+    string numberStr = Convert.ToString(number);
+    Console.WriteLine($"1- {numberStr}, {}");
+    int sum = 0;
+    for (int i = 0; i < numberStr.Length; i++)
+    {
+        sum = sum + Convert.ToInt32(numberStr[i]);
+        Console.WriteLine($"2- {sum}");
+    }
+    return sum;
+}
+int num = GetNumber("Введите число сумму цифр которого хотите получить: ");
+Console.WriteLine(num);
+
+int sumDigit = SumDigitsOfNumber(num);
+Console.WriteLine(sumDigit);
+
+
+
+//Console.WriteLine($"Сумма цифр введенного числа: {SumDigitsOfNumber(GetNumber("Введите число сумму цифр которого хотите получить: "))}");
