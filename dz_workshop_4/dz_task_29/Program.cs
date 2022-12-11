33ddd2dabdab56a3988265b12a502e7b71deceb7
Andrey B <a.borzenets@gmail.com>
2022-12-12 00:05:35 +0300

    add dz_task_29

--- dz_workshop_4/dz_task_29/Program.cs
+++ dz_workshop_4/dz_task_29/Program.cs
@@ -5,3 +5,27 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]
 */
+
+int[] GetArrayWithNumbers(int numberOfElements, int minDigit, int maxDigit)
+{
+    Random rnd = new Random();
+    int[] arrey = new int[numberOfElements];
+    for (int i = 0; i < numberOfElements; i++)
+    {
+        arrey[i] = rnd.Next(minDigit, maxDigit + 1);
+    }
+    return arrey;
+}
+
+
+void PrintArrey(int[] arrey)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrey.Length ; i++)
+    {
+        Console.Write($"{arrey[i]}, ");
+    }
+    Console.Write("]");
+} 
+
+PrintArrey(GetArrayWithNumbers(8, 1, 12));
