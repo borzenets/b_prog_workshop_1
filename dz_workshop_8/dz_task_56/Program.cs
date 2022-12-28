bdc8c308e97a726761a9d239ec63a334f77ed870
Andrey B <a.borzenets@gmail.com>
2022-12-29 02:50:05 +0300

    Added dz_task_56, dz_task_58

---
+++ dz_workshop_8/dz_task_56/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+Задача 56: Задайте прямоугольный двумерный массив.
+Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке
+и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+int[,] GetArrayRandomItemsTwoDimension(int xSizeArray = 5,
+                                        int ySizeArray = 5,
+                                        int startRndNum = 1,
+                                        int finishRndNum = 10)
+{
+    int[,] array = new int[xSizeArray, ySizeArray];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(startRndNum, finishRndNum);
+        }
+    }
+    return array;
+}
+
+void PrintTwoDimensionalArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int SearchRowMinSum(int[,] array)
+{
+    int indexRowMinSum = 0;
+    int sum = 0;
+    int minSum = 2147483647;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum = sum + array[i, j];
+        }
+        if (minSum > sum)
+        {
+            minSum = sum;
+            indexRowMinSum = i;
+        }
+        sum = 0;
+    }
+    return indexRowMinSum;
+}
+
+int[,] newArray = GetArrayRandomItemsTwoDimension();
+PrintTwoDimensionalArray(newArray);
+Console.WriteLine($"Строка с наименьшей суммой элементов: {SearchRowMinSum(newArray)}");
---
+++ dz_workshop_8/dz_task_58/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+Задача 58: Задайте две матрицы. Напишите программу,
+которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+
+void PrintTwoDimensionalArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] productOfArrays(int[,] oneArray, int[,] twoArray)
+{
+    int[,] newArray = new int[oneArray.GetLength(0), twoArray.GetLength(1)];
+
+    if (oneArray.GetLength(0) == twoArray.GetLength(1))
+    {
+        for (int i = 0; i < newArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < newArray.GetLength(1); j++)
+            {
+                for (int k = 0; k < newArray.GetLength(0); k++)
+                {
+                    newArray[i, j] += oneArray[i, k] * twoArray[k, j];
+                }
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("Массивы не могут быть перемноженны, "
+                        + "так как количество строк первого массива не равно "
+                        + "количеству столбцов второго массива.");
+    }
+    return newArray;
+}
+
+int[,] oneArray = { { 2, 4 }, { 3, 2 } };
+int[,] twoArray = { { 3, 4 }, { 3, 3 } };
+
+PrintTwoDimensionalArray(productOfArrays(oneArray, twoArray));
