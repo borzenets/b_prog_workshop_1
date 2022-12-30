f4b9cf3f38a08d1f50febdcf5018110513c7b042
Andrey B <a.borzenets@gmail.com>
2022-12-30 14:34:22 +0300

    Added dz_task_54

---
+++ dz_workshop_9/dz_task_64/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу,
+которая выведет все натуральные числа в промежутке от N до 1.
+Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+int GetNumberFromConsole(string message)
+{
+    int result;
+    Console.Write(message);
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result))
+            break;
+        else
+            Console.WriteLine("Вы ввели не число.");
+    }
+    return result;
+}
+
+void PrintIsNtoOne(int n)
+{
+    if (n != 0)
+    {
+    Console.Write($"{n}, ");
+    PrintIsNtoOne(n - 1);
+    }
+}
+
+PrintIsNtoOne(GetNumberFromConsole("Веедите положительное число: "));
