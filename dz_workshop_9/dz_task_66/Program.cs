dbfe6e005447fe9d4473884771bbceefe934f16f
Andrey B <a.borzenets@gmail.com>
2022-12-30 15:46:47 +0300

    Added dz_task_66

---
+++ dz_workshop_9/dz_task_66/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу,
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+
+int GetNumberFromConsole(string message)
+{
+    int result;
+    Console.Write(message);
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result))
+            break;
+        else
+            Console.WriteLine("Вы ввели не число.");
+    }
+    return result;
+}
+
+int SumIsMtoN(int m, int n, int sum=0)
+{
+    if (m <= n)
+    {
+        sum += m;
+        return SumIsMtoN(m +1, n, sum);
+    }
+    else
+        return sum;
+}
+
+int sum = SumIsMtoN(GetNumberFromConsole("Введите М: "), GetNumberFromConsole("Введите N: "));
+Console.WriteLine($"Сумма чисел от M до N равна: {sum}");
